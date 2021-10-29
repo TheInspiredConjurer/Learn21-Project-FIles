276b294b95765425489210e6ee569cb9889f858d
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2021-10-29 15:33:10 +0530

    Create Personal Portfolio App

---
+++ Portfolio/Portfolio_Builder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PortfolioBuilderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Portfolio_Builder'
---
+++ Portfolio/Portfolio_Builder/views.py
@@ -0,0 +1,9 @@
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request) :
+    return render(request, 'index.html')
+def portfoliodetails(request) :
+    return render(request, 'portfolio-details.html')
