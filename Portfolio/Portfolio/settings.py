6d1e5787d975d1dccbe3eae0b8f30b2e0fd89e95
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2021-11-19 15:28:24 +0530

    Pushing for Aayush to check errors

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Portfolio_Builder'
+    'Portfolio_Builder',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
