d7d696b340abf2b6fb0d7488027c85f78da55cd6
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2021-11-19 17:43:57 +0530

    RIP project. I will never forget you, ayush >:(

--- Portfolio/Portfolio_Builder/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortfolioBuilderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Portfolio_Builder'
--- Portfolio/Portfolio_Builder/views.py
+++
@@ -1,9 +0,0 @@
-from django.http.request import HttpRequest
-from django.http.response import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def index(request) :
-    return render(request, 'index.html')
-def portfoliodetails(request) :
-    return render(request, 'portfolio-details.html')
