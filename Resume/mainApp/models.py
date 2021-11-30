91ef330ad389dbd3bc780c2c89fc55cb6f86e167
The Inspired Conjurer <ramyakrohanmohanty@gmail.com>
2021-11-30 21:13:57 +0530

    Create models

--- Resume/mainApp/models.py
+++ Resume/mainApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    subject_line = models.CharField(max_length=200)
+    message = models.TextField()
